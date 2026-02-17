3b07ebd89835a300fb5bcd18daade2ae4fa69985
Torenia.Fournieri <46834896+ToreniaFournieri@users.noreply.github.com>
2026-02-17 18:13:08 +0900

    Adjust item type amplifiers for balance nerfs

--- src/data/items.ts
+++ src/data/items.ts
@@ -78,13 +78,13 @@ const TYPE_AMPLIFIERS: Record<ItemCategory, number> = {
   robe: 1.0,
   shield: 1.0,
   sword: 1.2,
-  katana: 2.4,
+  katana: 1.92,
   gauntlet: 1.0,
   arrow: 0.67,
-  bolt: 1.33,
+  bolt: 1.07,
   archery: 1.0,
   wand: 0.5,
-  grimoire: 1.0,
+  grimoire: 0.8,
   catalyst: 1.0,
 };
 
