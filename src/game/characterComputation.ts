8953958b6a913508dbe174407f0d4986ba9c76a6
Torenia.Fournieri <46834896+ToreniaFournieri@users.noreply.github.com>
2026-02-19 10:53:49 +0900

    Update characterComputation.ts

--- src/game/characterComputation.ts
+++ src/game/characterComputation.ts
@@ -589,7 +589,7 @@ function getAbilityDescription(id: AbilityId, level: number): string {
     squander: () => '宴会で消費するゴールドが2倍になる',
     tithe: () => '祈り時に寄付額へ探検利益の+10%を加算',
     seeker: () => '魔導書の効果増加(レベル毎に0.25%)',
-    resurrect: () => '1戦闘につき1回のみ即死ダメージをHP1残して耐える',
+    resurrect: () => '即死ダメージをHP1残して耐える',
   };
   return descriptions[id](level);
 }
