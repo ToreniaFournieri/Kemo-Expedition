c0a1c02b01c1a98b85cdd3c8e1be5fb1ecf96cc4
Torenia.Fournieri <46834896+ToreniaFournieri@users.noreply.github.com>
2026-04-02 11:12:20 +0900

    Fix missing c.* base bonuses on item generation

--- src/data/items.ts
+++ src/data/items.ts
@@ -412,7 +412,6 @@ function createItem(
       break;
     case 'shield':
       item.partyHP = calculateStat(basePower, amplifier * 3.0);
-      if (rarity !== 'common') item.evasionBonus = TIER_F_BONUS[getTierIndex(bonusTierF)];
       if (rarity === 'eliteRare' && eliteSource !== 'A') {
         item.physicalDefense = calculateStat(basePower, amplifier * 0.2);
         item.magicalDefense = calculateStat(basePower, amplifier * 0.2);
@@ -515,13 +514,17 @@ function createItem(
   }
   if (rarity !== 'bossRare' && rarity !== 'mythicRare') {
     const bonusTier = getTierIndex(bonusTierF);
-    if (template.category === 'armor') item.physicalDefense = (item.physicalDefense || 0) + TIER_F_BONUS[bonusTier];
-    if (template.category === 'robe') item.magicalDefense = (item.magicalDefense || 0) + TIER_F_BONUS[bonusTier];
+    const addCBonus = (type: 'physical_defense' | 'magical_defense', value: number): void => {
+      item.bonuses = [...(item.bonuses ?? []), { type, value }];
+    };
+
+    if (template.category === 'armor') addCBonus('physical_defense', TIER_F_BONUS[bonusTier]);
+    if (template.category === 'robe') addCBonus('magical_defense', TIER_F_BONUS[bonusTier]);
     if (template.category === 'shield') item.evasionBonus = (item.evasionBonus || 0) + TIER_F_BONUS[bonusTier];
     if (template.category === 'sword') item.accuracyBonus = (item.accuracyBonus || 0) + TIER_N_BONUS[getTierIndex(bonusTierN)];
-    if (template.category === 'gauntlet') item.physicalDefense = (item.physicalDefense || 0) + TIER_N_BONUS[getTierIndex(bonusTierN)];
+    if (template.category === 'gauntlet') addCBonus('physical_defense', TIER_N_BONUS[getTierIndex(bonusTierN)]);
     if (template.category === 'archery') item.accuracyBonus = (item.accuracyBonus || 0) + TIER_N_BONUS[getTierIndex(bonusTierN)];
-    if (template.category === 'catalyst') item.magicalDefense = (item.magicalDefense || 0) + TIER_N_BONUS[getTierIndex(bonusTierN)];
+    if (template.category === 'catalyst') addCBonus('magical_defense', TIER_N_BONUS[getTierIndex(bonusTierN)]);
     if (template.category === 'katana' || template.category === 'bolt' || template.category === 'grimoire') {
       item.penetBonus = (item.penetBonus || 0) + TIER_P_BONUS[getTierIndex(bonusTierP)];
     }
