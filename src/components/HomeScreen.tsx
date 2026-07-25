e65809d7dfa4438c44396c5b8ccedfea212eed1d
Torenia.Fournieri <46834896+ToreniaFournieri@users.noreply.github.com>
2026-07-25 13:32:33 +0900

    Update HomeScreen.tsx

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -6972,7 +6972,7 @@ function PartyTab({
                   <div
                     aria-hidden="true"
                     className={`pointer-events-none absolute bottom-0 left-1/2 z-0 h-[240%] -translate-x-1/2 bg-contain bg-bottom bg-no-repeat ${
-                      previewMimorianEnemyImageSrc ? 'w-[160%]' : 'w-[220%]'
+                      previewMimorianEnemyImageSrc ? 'w-[180%]' : 'w-[220%]'
                     }`}
                     style={{ backgroundImage: `url(${previewImageSrc})` }}
                   />
